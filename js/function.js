5b16bb88ee247d062f2f29102acbccd8602f382e
Jonathan 'Slaals' Serra <jonathan.serra@utt.fr>
2015-09-23 11:59:02 +0200

    It's now possible to change the number of movies displayed by page

--- js/function.js
+++ js/function.js
@@ -1,26 +1,14 @@
 var resources = "https://dev-api.streamnation.com/api/v1/movies?with_facets=false&page=1&sort_by=new_content&order=desc&watched=false&currently_playing_first=true&per_page=10&cover=low&fields=[%22name%22%2c%22cover%22]&x_api_version=2.0&auth_token=ap84WKgZ5rgy8Dmkamwe";
-var nbMoviesPage = 3;
+var nbMoviesPage = 1;
 
 var movies = [];
 
-$(document).ready(function() {
+$( document ).ready( function() {
 	
 	$.getJSON( resources, function( data ) {
 		movies = data.movies;
 
-		var nbPages = Math.ceil( movies.length / nbMoviesPage );
-
-		for( i = 1; i <= nbPages; i++ ) {
-			$( '#pages' ).append( '<li class="page-link"><a>' + i + '</a></li>' );
-		}
-
-		$( '#pages' ).children().first().addClass( 'selected' );
-
-		$( '#pages li' ).on( 'click', function() {
-			selectPage( $(this) );
-		});
-
-		updateMoviesView( 0 );
+		definePagesLink( 0 );
 	});
 
 	$( '#movies-page' ).val( nbMoviesPage );
@@ -32,13 +20,49 @@ $(document).ready(function() {
 	$( '#page-right' ).on( 'click', function() {
 		selectPage( $( '#pages .page-link.selected' ).next() );
 	});
+
+	$( '#movies-page' ).on( 'keyup', function() {
+		var inputVal = $( this ).val();
+
+		if( ( /^\d+$/.test( inputVal ) === false ) || ( inputVal <= 0 ) ) {
+			// Alert (flash bag)
+		} else {
+			var index = $( '#pages .page-link.selected' ).index();
+
+			nbMoviesPage = parseInt( inputVal );
+
+			definePagesLink( index );
+
+			updateMoviesView( index );
+		}
+	});
 });
 
-function selectPage( newPage ) {
-	index = 0;
-	pages = $( '#pages' ).children();
+function definePagesLink( index ) {
+	var pages = $( '#pages' );
+	var nbPages = Math.ceil( movies.length / nbMoviesPage );
 
-	current = $( '#pages .page-link.selected' );
+	pages.empty();
+
+	for( i = 1; i <= nbPages; i++ ) {
+		$( '#pages' ).append( '<li class="page-link"><a>' + i + '</a></li>' );
+	}
+
+	if( pages.children().index( index ) < 0 ) {
+		selectPage( pages.children().first() );
+	} else {
+		selectPage( pages.children().eq( index ) );
+	}
+
+	$( '#pages li' ).on( 'click', function() {
+		selectPage( $(this) );
+	});
+}
+
+function selectPage( newPage ) {
+	var index = 0;
+	var pages = $( '#pages' ).children();
+	var current = $( '#pages .page-link.selected' );
 
 	pages.removeClass( 'selected' );
 
@@ -58,11 +82,11 @@ function selectPage( newPage ) {
 }
 
 function updateMoviesView( index ) {
-	container = $( '#movies-view' );
+	var container = $( '#movies-view' );
+	var from = nbMoviesPage * index;
+	var to = from + nbMoviesPage;
 
-	from = nbMoviesPage * index;
-
-	to = from + nbMoviesPage;
+	alert("from : " + from + " - to : " + to );
 
 	if( to > movies.length ) {
 		to = movies.length;
@@ -76,6 +100,6 @@ function updateMoviesView( index ) {
 		movie = container.children().last();
 
 		movie.append( '<img class="cover" src="' + movies[i].cover + '"></img>' );
-		movie.append( '<h2>' + movies[i].name + '</h2>');
+		movie.append( '<h3>' + movies[i].name + '</h3>');
 	}
 }
