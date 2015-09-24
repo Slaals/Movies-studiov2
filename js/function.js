1704f4c4bd902f1bd6cc9f6e1e4ed52114b1d939
Jonathan 'Slaals' Serra <jonathan.serra@utt.fr>
2015-09-24 14:04:16 +0200

    Update by scrolling + implementation of DOM recycle

--- js/function.js
+++ js/function.js
@@ -1,6 +1,12 @@
-const ELEMENT_LIMIT = 10;
+const LIMIT_ELEMENT = 10;
+const PER_PAGE = 10;
 
 var page = 1;
+
+var nbElementsDisplayed = 0;
+
+var firstElementsRemoved = [];
+var firstElementsRemoved = [];
 
 /*
  * <div class="movie">
@@ -20,32 +26,82 @@ var transform = { tag:'div', class:'movie', children:[
 $( document ).ready( function() {
 	getDataPage( );
 
+	$( window ).scroll( function() {
+		if( $( window ).scrollTop() == 0 ) {
+			$( '#movies-view' ).regenerateFirstNode();
+		}
+	});
+
 	$( window ).scroll( function( ) {
 		if( $( window ).height() + $( window ).scrollTop() == $( document ).height() ) {
-			page += 1;
 			getDataPage( )
 		}
 	});
 });
 
 function getDataPage( ) {
-	var res = "https://dev-api.streamnation.com/api/v1/movies?with_facets=false&page=" + page + "&sort_by=new_content&order=desc&watched=false&currently_playing_first=true&per_page=10&cover=low&fields=[%22name%22%2c%22cover%22]&x_api_version=2.0&auth_token=ap84WKgZ5rgy8Dmkamwe";
+	var res = "https://dev-api.streamnation.com/api/v1/movies?with_facets=false&page=" + page + "&sort_by=new_content&order=desc&watched=false&currently_playing_first=true&per_page=" + PER_PAGE +"&cover=low&fields=[%22name%22%2c%22cover%22]&x_api_version=2.0&auth_token=ap84WKgZ5rgy8Dmkamwe";
 	return $.getJSON( res, function( data ) {
-		updateMoviesView( data.movies );
+		if( data.movies.length > 0 ) { // Page isn't full so it's considered to be the end
+			var movies = data.movies;
+
+			page += 1;
+
+			for( i = 0; i < movies.length; i++ ) {
+				$( '#movies-view' ).append( json2html.transform( movies[i], transform ) );
+				nbElementsDisplayed += 1;
+			}
+
+			updateMoviesView( );
+		}
 	});
 }
 
 /*
  * Display the movie(s)
  */
-function updateMoviesView( movies ) {
-	var container = $( '#movies-view' );
+function updateMoviesView( ) {
+	if( nbElementsDisplayed > LIMIT_ELEMENT ) {
+		var nbElementsToRemove = nbElementsDisplayed - LIMIT_ELEMENT;
+		for( i = 0; i < nbElementsToRemove; i++ ) {
+			$( '#movies-view' ).removeFirstNode();
+		}
+	}
+}
 
-	/* if( container.children().length > ELEMENT_LIMIT ) {
-		container.children().first().remove();
-	}*/
+$.fn.removeFirstNode = function( ) {
+	var node = $( this ).children().first();
 
-	for( i = 0; i < movies.length; i++ ) {
-		container.append( json2html.transform( movies[i], transform ) );
+	firstElementsRemoved.push( node.clone() );
+
+	node.remove();
+
+	nbElementsDisplayed -= 1;
+}
+
+$.fn.regenerateFirstNode = function( ) {
+	var firstElement = firstElementsRemoved.length;
+	for( i = firstElement; i >= 0; i-- ) {
+		$( '#movies-view' ).prepend( firstElementsRemoved[i] );
+		delete firstElementsRemoved[i];
+		nbElementsDisplayed += 1;
+	}
+}
+
+$.fn.removeLastNode = function( ) {
+	var node = $( this ).children().last();
+
+	lastElementsRemoved.push( node.clone() );
+
+	node.remove();
+
+	nbElementsDisplayed -= 1;
+}
+
+$.fn.regenerateLastNode = function( ) {
+	for( i = 0; i < lastElementsRemoved.length; i++ ) {
+		$( '#movies-view' ).append( lastElementsRemoved[i] );
+		delete lastElementsRemoved[i];
+		nbElementsDisplayed += 1;
 	}
 }
