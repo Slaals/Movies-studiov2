92fa63a346b13a26a2e9595ffa6be4ba45f17650
Jonathan 'Slaals' Serra <jonathan.serra@utt.fr>
2015-09-25 00:20:34 +0200

    Stub finished = > DOM recycle

--- js/function.js
+++ js/function.js
@@ -1,12 +1,17 @@
-const LIMIT_ELEMENT = 10;
+const NB_ELEMENT = 10;
 const PER_PAGE = 10;
 
 var page = 1;
 
 var nbElementsDisplayed = 0;
 
-var firstElementsRemoved = [];
-var firstElementsRemoved = [];
+var firstElementsStubbed = [];
+var lastElementsStubbed = [];
+
+var scrollMovement = false;
+
+// TODO gérer avec le scroll ( delete en avant ou en arrière ... ) => STUB en utilisant la classe movie
+// http://engineering.linkedin.com/linkedin-ipad-5-techniques-smooth-infinite-scrolling-html5
 
 /*
  * <div class="movie">
@@ -27,21 +32,69 @@ $( document ).ready( function() {
 	getDataPage( );
 
 	$( window ).scroll( function() {
-		if( $( window ).scrollTop() == 0 ) {
-			$( '#movies-view' ).regenerateFirstNode();
-		}
+		if( $( window ).height() + $( window ).scrollTop() == $( document ).height() ) {
+			getDataPage();
+		} 
+
+		scrollMovement = true;
 	});
 
-	$( window ).scroll( function( ) {
-		if( $( window ).height() + $( window ).scrollTop() == $( document ).height() ) {
-			getDataPage( )
+	setInterval( function( ) {
+		if( scrollMovement ) {
+			updateMovies();
 		}
-	});
+	}, 1000 );
+
 });
+
+function updateMovies( ) {
+	var topNode = $( '#movies-view' ).children( '.movie' ).first();
+	var bottomNode = $( '#movies-view' ).children( '.movie' ).last();
+	var limitDisplayTop = $( window ).scrollTop() - $( window ).height();
+	var limitDisplayBottom = $( window ).scrollTop() + $( window ).height() * 1.5;
+
+	if( topNode.offset().top < limitDisplayTop ) {
+		for( i = 0; i < NB_ELEMENT; i++) {
+			var node = $( '#movies-view' ).children( '.movie' ).first();
+			firstElementsStubbed.push( node.clone() );
+			node.replaceWith( "<div class='first stub'></div>" );
+		}
+
+		var firstElement = lastElementsStubbed.length - 1;
+		for( i = firstElement; i >= lastElementsStubbed.length - NB_ELEMENT; i-- ) {
+			var node = $( '#movies-view' ).children( '.last.stub' ).first();
+			node.replaceWith( lastElementsStubbed[i] );
+
+			delete lastElementsStubbed[i];
+			nbElementsDisplayed += 1;
+		}
+	} 
+
+	if( bottomNode.offset().top > limitDisplayBottom ) {
+		var node;
+		for( i = 0; i < NB_ELEMENT; i++) {
+			node = $( '#movies-view' ).children( '.movie' ).last();
+			lastElementsStubbed.push( node.clone() );
+			node.replaceWith( "<div class='last stub'></div>" );
+		}
+
+		var firstElement = firstElementsStubbed.length - 1;
+		for( i = firstElement; i >= firstElement - NB_ELEMENT; i-- ) {
+			var node = $( '#movies-view' ).children( '.first.stub' ).last();
+			node.replaceWith( firstElementsStubbed[i] );
+
+			delete firstElementsStubbed[i];
+			nbElementsDisplayed += 1;
+		}
+
+	} 
+
+}
 
 function getDataPage( ) {
 	var res = "https://dev-api.streamnation.com/api/v1/movies?with_facets=false&page=" + page + "&sort_by=new_content&order=desc&watched=false&currently_playing_first=true&per_page=" + PER_PAGE +"&cover=low&fields=[%22name%22%2c%22cover%22]&x_api_version=2.0&auth_token=ap84WKgZ5rgy8Dmkamwe";
-	return $.getJSON( res, function( data ) {
+
+	$.getJSON( res, function( data ) {
 		if( data.movies.length > 0 ) { // Page isn't full so it's considered to be the end
 			var movies = data.movies;
 
@@ -51,57 +104,10 @@ function getDataPage( ) {
 				$( '#movies-view' ).append( json2html.transform( movies[i], transform ) );
 				nbElementsDisplayed += 1;
 			}
-
-			updateMoviesView( );
-		}
+		} 
 	});
 }
 
-/*
- * Display the movie(s)
- */
-function updateMoviesView( ) {
-	if( nbElementsDisplayed > LIMIT_ELEMENT ) {
-		var nbElementsToRemove = nbElementsDisplayed - LIMIT_ELEMENT;
-		for( i = 0; i < nbElementsToRemove; i++ ) {
-			$( '#movies-view' ).removeFirstNode();
-		}
-	}
-}
-
-$.fn.removeFirstNode = function( ) {
-	var node = $( this ).children().first();
-
-	firstElementsRemoved.push( node.clone() );
-
-	node.remove();
-
-	nbElementsDisplayed -= 1;
-}
-
-$.fn.regenerateFirstNode = function( ) {
-	var firstElement = firstElementsRemoved.length;
-	for( i = firstElement; i >= 0; i-- ) {
-		$( '#movies-view' ).prepend( firstElementsRemoved[i] );
-		delete firstElementsRemoved[i];
-		nbElementsDisplayed += 1;
-	}
-}
-
-$.fn.removeLastNode = function( ) {
-	var node = $( this ).children().last();
-
-	lastElementsRemoved.push( node.clone() );
-
-	node.remove();
-
-	nbElementsDisplayed -= 1;
-}
-
-$.fn.regenerateLastNode = function( ) {
-	for( i = 0; i < lastElementsRemoved.length; i++ ) {
-		$( '#movies-view' ).append( lastElementsRemoved[i] );
-		delete lastElementsRemoved[i];
-		nbElementsDisplayed += 1;
-	}
+$.fn.stub = function( classes ) {
+	$( this ).replaceWith( '<div class="' + classes + '"></div>' );
 }
