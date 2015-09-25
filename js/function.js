37ca3d3992d3a191137cde64a39a7ed4eaa71b1b
Jonathan Serra <jonathan.serra@utt.fr>
2015-09-25 02:10:34 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -103,7 +103,7 @@ function getDataPage( ) {
 	var res = "https://dev-api.streamnation.com/api/v1/movies?with_facets=false&page=" + page + "&sort_by=new_content&order=desc&watched=false&currently_playing_first=true&per_page=10&cover=low&fields=[%22name%22%2c%22cover%22]&x_api_version=2.0&auth_token=ap84WKgZ5rgy8Dmkamwe";
 
 	$.getJSON( res, function( data ) {
-		if( data.movies.length > 0 ) { // Page isn't full so it's considered to be the end
+		if( data.movies.length > 0 ) {
 			var movies = data.movies;
 
 			page += 1;
